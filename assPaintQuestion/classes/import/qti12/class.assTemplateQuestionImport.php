a91581a65f31369a038aaf10276ee92aa540dfb3
Christoph Jobst <cjobst@wifa.uni-leipzig.de>
2014-10-21 10:57:29 +0200

    Initial commit

---
+++ assPaintQuestion/classes/import/qti12/class.assTemplateQuestionImport.php
@@ -0,0 +1,130 @@
+<?php
+
+include_once "./Modules/TestQuestionPool/classes/import/qti12/class.assQuestionImport.php";
+
+/**
+* Class for PaintQuestion import
+*
+* @author Yves Annanias <yves.annanias@llz.uni-halle.de>
+* @version	$Id: $
+* @ingroup 	ModulesTestQuestionPool
+*/
+class assPaintQuestionImport extends assQuestionImport
+{
+	/**
+	* Creates a question from a QTI file
+	*
+	* Receives parameters from a QTI parser and creates a valid ILIAS question object
+	*
+	* @param object $item The QTI item object
+	* @param integer $questionpool_id The id of the parent questionpool
+	* @param integer $tst_id The id of the parent test if the question is part of a test
+	* @param object $tst_object A reference to the parent test object
+	* @param integer $question_counter A reference to a question counter to count the questions of an imported question pool
+	* @param array $import_mapping An array containing references to included ILIAS objects
+	* @access public
+	*/
+	function fromXML(&$item, $questionpool_id, &$tst_id, &$tst_object, &$question_counter, &$import_mapping)
+	{
+		global $ilUser;
+
+		// empty session variable for imported xhtml mobs
+		unset($_SESSION["import_mob_xhtml"]);
+		$presentation = $item->getPresentation(); 
+		$duration = $item->getDuration();
+		$now = getdate();
+		$created = sprintf("%04d%02d%02d%02d%02d%02d", $now['year'], $now['mon'], $now['mday'], $now['hours'], $now['minutes'], $now['seconds']);
+
+		$feedbacksgeneric = array();
+
+		$this->object->setTitle($item->getTitle());
+		$this->object->setComment($item->getComment());
+		$this->object->setAuthor($item->getAuthor());
+		$this->object->setOwner($ilUser->getId());
+		$this->object->setQuestion($this->object->QTIMaterialToString($item->getQuestiontext()));
+		$this->object->setObjId($questionpool_id);
+		$this->object->setEstimatedWorkingTime($duration["h"], $duration["m"], $duration["s"]);		
+		$this->object->setPoints($item->getMetadataEntry("points"));
+		
+		$this->object->saveToDb('', false);
+		
+		/*
+		$image =& base64_decode($questionimage["content"]);
+		$imagepath = $this->object->getImagePath();
+		if (!file_exists($imagepath))
+		{
+			include_once "./Services/Utilities/classes/class.ilUtil.php";
+			ilUtil::makeDirParents($imagepath);
+		}
+		$imagepath .=  $questionimage["label"];
+		$fh = fopen($imagepath, "wb");
+		if ($fh == false)
+		{
+//									global $ilErr;
+//									$ilErr->raiseError($this->object->lng->txt("error_save_image_file") . ": $php_errormsg", $ilErr->MESSAGE);
+//									return;
+		}
+		else
+		{
+			$imagefile = fwrite($fh, $image);
+			fclose($fh);
+		}
+		*/
+
+		// handle the import of media objects in XHTML code
+		$questiontext = $this->object->getQuestion();
+		if (is_array($_SESSION["import_mob_xhtml"]))
+		{
+			include_once "./Services/MediaObjects/classes/class.ilObjMediaObject.php";
+			include_once "./Services/RTE/classes/class.ilRTE.php";
+			foreach ($_SESSION["import_mob_xhtml"] as $mob)
+			{
+				if ($tst_id > 0)
+				{
+					include_once "./Modules/Test/classes/class.ilObjTest.php";
+					$importfile = ilObjTest::_getImportDirectory() . '/' . $mob["uri"];
+				}
+				else
+				{
+					include_once "./Modules/TestQuestionPool/classes/class.ilObjQuestionPool.php";
+					$importfile = ilObjQuestionPool::_getImportDirectory() . '/' . $mob["uri"];
+				}
+				$media_object =& ilObjMediaObject::_saveTempFileAsMediaObject(basename($importfile), $importfile, FALSE);
+				ilObjMediaObject::_saveUsage($media_object->getId(), "qpl:html", $this->object->getId());
+				$questiontext = str_replace("src=\"" . $mob["mob"] . "\"", "src=\"" . "il_" . IL_INST_ID . "_mob_" . $media_object->getId() . "\"", $questiontext);
+/*
+				foreach ($feedbacksgeneric as $correctness => $material)
+				{
+					$feedbacksgeneric[$correctness] = str_replace("src=\"" . $mob["mob"] . "\"", "src=\"" . "il_" . IL_INST_ID . "_mob_" . $media_object->getId() . "\"", $material);
+				}
+*/				
+			}
+		}
+		$this->object->setQuestion(ilRTE::_replaceMediaObjectImageSrc($questiontext, 1));
+			
+		$this->object->saveToDb('', true);
+	/*
+		if (count($item->suggested_solutions))
+		{
+			foreach ($item->suggested_solutions as $suggested_solution)
+			{
+				$this->object->setSuggestedSolution($suggested_solution["solution"]->getContent(), $suggested_solution["gap_index"], true);
+			}
+			$this->object->saveToDb();
+		}
+	*/
+		if ($tst_id > 0)
+		{
+			$q_1_id = $this->object->getId();
+			$question_id = $this->object->duplicate(true);
+			$tst_object->questions[$question_counter++] = $question_id;
+			$import_mapping[$item->getIdent()] = array("pool" => $q_1_id, "test" => $question_id);
+		}
+		else
+		{
+			$import_mapping[$item->getIdent()] = array("pool" => $this->object->getId(), "test" => 0);
+		}
+	}
+}
+
+?>
